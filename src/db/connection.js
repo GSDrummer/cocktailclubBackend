22a48c34c691e9e4f22e07c0057879046fddad11
Luke Ellwood <ellwood.luke@gmail.com>
2021-06-15 16:07:14 +0100

    added some console logs to show server connecting to DB

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,12 +2,15 @@ const mongoose = require(`mongoose`);
 require("dotenv").config();
 
 const connection = async () => {
+  console.log(process.env.MONGO_URI);
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
+      useCreateIndex: true,
     });
+    console.log("WAHEY");
   } catch (error) {
     console.log(error);
   }
