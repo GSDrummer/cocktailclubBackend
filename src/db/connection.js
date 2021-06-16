3ef374e40e2dc16e2b749d33aea45a728a03be8c
Luke Ellwood <ellwood.luke@gmail.com>
2021-06-16 14:18:04 +0100

    removed console log on connection

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,7 +2,6 @@ const mongoose = require(`mongoose`);
 require("dotenv").config();
 
 const connection = async () => {
-  console.log(process.env.MONGO_URI);
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
