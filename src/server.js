ff463727fd3cc8b008737286e11a30ce73bd0548
Luke Ellwood <ellwood.luke@gmail.com>
2021-06-16 10:05:21 +0100

    working back end

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const { userRouter } = require("./models/user.routes");
 
 app.use(cors());
 app.use(express.json());
+app.use(userRouter);
 
 app.listen(port, () => {
   console.log(`Server listening on ${port}`);
