b56ef313890c49a81d572c0d5ec577df455c07e0
Luke Ellwood <ellwood.luke@gmail.com>
2021-06-24 16:52:59 +0100

    deployment

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcryptjs");
+const { User } = require("../models/user.model");
+require("dotenv").config();
 
 exports.hashPassword = async (req, res, next) => {
   if ("password" in req.body) {
